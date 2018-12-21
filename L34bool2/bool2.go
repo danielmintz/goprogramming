80b74db091875a5b8ee42606909aa75db6456629
Daniel Mintz <danielmintz@Daniels-MacBook-Pro.local>
2018-12-21 18:15:24 +0000

    bool&int

---
+++ L34bool2/bool2.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 42
+	y := 7
+	fmt.Println(x == y)
+	fmt.Println(x >= y)
+	fmt.Println(y <= x)
+	fmt.Println(x != y)
+
+}
---
+++ L35intfloat64/L35intfloat64.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	x := 42     // this is an int which is a whole number
+	y := 42.345 // type here is a float62 for decimalsknown a sloating or real number
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", y)
+	fmt.Println(runtime.GOOS) //operating systm of computer
+	fmt.Println(runtime.GOARCH)
+
+}
+
+// nb uint would be a positive wherea int can be positive and negative
