87980a24229e41ed0dac1b2b81e12760f0d4748f
Daniel Mintz <danielmintz@Daniels-MacBook-Pro.local>
2018-12-26 19:29:31 +0000

    loops

--- L50Initconditionalpost/L50initconditionalpost.go
+++ L50Initconditionalpost/L50initconditionalpost.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	//init; conditional; post (for is another way of saying while)
+	//for init; conditional; post {}(for is another way of saying while)
 	for a := 0; a <= 100; a++ {
 
 		fmt.Println(a)
