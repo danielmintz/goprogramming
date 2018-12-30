1dbee13d41c4a373aa62031d8a1ffed2fe7d5de5
Daniel Mintz <danielmintz@Daniels-MacBook-Pro.local>
2018-12-30 23:20:47 +0000

    slice2

---
+++ L75appendtoaslice/L75appendtoaslice.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{5, 4, 6, 7, 8}
+	fmt.Println(x)
+	x = append(x, 12, 13, 14, 15)
+	fmt.Println(x)
+	y := []int{77, 88, 99, 101}
+	fmt.Println(y)
+	x = append(x, y...)
+	fmt.Println(x)
+}
+
+//To append values to a slice, we use the special built in function append. This function returns a slice of the same type.
+//note when curly {} and when brackets ()
+//note ... (include everything) research variadic function.. an infinite number
--- Play/play.go
+++ Play/play.go
@@ -5,4 +5,10 @@ import "fmt"
 func main() {
 	x := []int{5, 4, 6, 7, 8}
 	fmt.Println(x)
+	x = append(x, 12, 13, 14, 15)
+	fmt.Println(x)
+	y := []int{77, 88, 99, 101}
+	fmt.Println(y)
+	x = append(x, y...)
+	fmt.Println(x)
 }
