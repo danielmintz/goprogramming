85244ba8d0dcda3d513cb69a1630d7c8482db065
Daniel Mintz <danielmintz@Daniels-MacBook-Pro.local>
2019-01-12 09:44:26 +0000

    foldersetup

---
+++ Section14Functions/L101syntax/L101syntax.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+//we call our func and pass in arguments (if any)
+func main() {
+	foo()
+	bar("James")
+	s1 := woo("Miss MoneyPenny")
+	fmt.Println(s1)
+	x, y := mouse("Ian", "Fleming") // 2 variables one is the string other the bool
+	fmt.Println(x)
+	fmt.Println(y)
+}
+
+//we define our func with parameters (if any)
+func foo() {
+	fmt.Println("Hey this is foo")
+}
+func bar(s string) {
+	fmt.Println("Hello", s)
+}
+
+// using return
+func woo(st string) string {
+	return fmt.Sprint("Hello woo, this is ", st)
+}
+
+//using multiple returns
+func mouse(fn string, ln string) (string, bool) {
+	a := fmt.Sprintln(fn, ln, `, says "hello" `)
+	b := true
+	return a, b
+}
+
+//Syntax
+//func (receiver) identifier(parameters) (returns) { code }
+//know the difference between parameters and arguments
+//everthing in go in PASS BY VALUE
