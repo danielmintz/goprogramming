db4c27a993067f17adea15ccfb606c8b19d7637d
Daniel Mintz <danielmintz@Daniels-MacBook-Pro.local>
2019-02-02 22:49:21 +0000

    Pointers

--- Play/play.go
+++ Play/play.go
@@ -2,59 +2,23 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-type square struct {
-	length float64
-}
-
-type circle struct {
-	radius float64
-}
-
-func (s square) area() float64 {
-	return s.length * s.length
-}
-
-func (s circle) area() float64 {
-	return math.Pi * s.radius * s.radius
-}
-
-type shape interface {
-	area() float64
-}
-
-func info(s shape) {
-	fmt.Println("this is the area:", s.area())
-
+type person struct {
+	name string
 }
 
 func main() {
 
-	squ := square{
-		length: 12.2,
+	p1 := person{
+		name: "Daniel",
 	}
+	fmt.Println(p1)
 
-	cir := circle{
-		radius: 8.1,
-	}
-
-	info(squ)
-	info(cir)
-
+	changeMe(&p1)
+	fmt.Println(p1)
 }
 
-//func (r receiver) identifier(parameters) (returns) { code }
-
-// create a type SQUARE
-// create a type CIRCLE
-// attach a method to each that calculates AREA and returns it
-// circle area= π r 2
-// square area = L * W
-// create a type SHAPE that defines an interface as anything that has the AREA method
-// create a func INFO which takes type shape and then prints the area
-// create a value of type square
-// create a value of type circle
-// use func info to print the area of square
-// use func info to print the area of circle
+func changeMe(p *person) {
+	(*p).name = "Debra"
+}
---
+++ Todd Go Course/Section18Applications/L130JSONMarshall/L130JSONMarshall.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First string
+	Last  string
+	Age   int
+}
+
+func main() {
+
+	p1 := person{
+		First: "James",
+		Last:  "Bond",
+		Age:   27,
+	}
+
+	p2 := person{
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
+	}
+
+	people := []person{p1, p2}
+	fmt.Println(people)
+
+	bs, err := json.Marshal(people)
+	if err != nil {
+		fmt.Println("error", err)
+	}
+	fmt.Println(string(bs))
+	//os.Stdout.Write(bs)
+}
+
+//func Marshal(v interface{}) ([]byte, error)
