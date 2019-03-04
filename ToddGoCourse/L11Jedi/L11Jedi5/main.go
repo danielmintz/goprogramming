c0266d09925835b1817fd69463a3883b3858362f
Daniel Mintz <danielmintz@Daniels-MacBook-Pro.local>
2019-03-04 20:23:55 +0000

    testing

---
+++ ToddGoCourse/L11Jedi/L11Jedi5/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"goprogramming/ToddGoCourse/L11Jedi/L11Jedi5/math"
+)
+
+func main() {
+	xs := []float64{1, 2, 3, 4}
+	avg := math.Average(xs)
+	fmt.Println(avg)
+}
