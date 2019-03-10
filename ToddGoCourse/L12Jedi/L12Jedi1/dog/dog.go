d2418839f8e7f08809f1fc62c2645f1ced5774b8
Daniel Mintz <danielmintz@Daniels-MacBook-Pro.local>
2019-03-10 09:27:41 +0000

    jeeze

---
+++ ToddGoCourse/L12Jedi/L12Jedi1/dog/dog.go
@@ -0,0 +1,7 @@
+// Package dog is a package for all things dog related
+package dog
+
+// Years is a function that takes human years and turns them into dog years (1 human year = 7 dog years).
+func Years(n int) int {
+	return n * 7
+}
---
+++ ToddGoCourse/L12Jedi/L12Jedi1/main.go
@@ -0,0 +1,19 @@
+package main
+
+import("goprogramming/ToddGoCourse/L12Jedi/L12Jedi1/dog/"
+"fmt")
+
+type Canine struct{
+	name string
+	age int
+}
+func main() {
+
+	dog1 := Canine{
+		name: "Fido",
+		age: dog.Years(7)
+	}
+
+fmt.Println(dog1)
+
+}
