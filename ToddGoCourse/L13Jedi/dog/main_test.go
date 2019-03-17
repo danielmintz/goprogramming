76d227f91be878531bdbd04c2b37b1c0525ae988
Daniel Mintz <danielmintz@Daniels-MacBook-Pro.local>
2019-03-17 09:58:43 +0000

    go get em

--- ToddGoCourse/L13Jedi/dog/main_test.go
+++ ToddGoCourse/L13Jedi/dog/main_test.go
@@ -1,6 +1,33 @@
 package dog
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func BenchmarkYears(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Years(1)
+	}
+}
+
+func BenchmarkYearsTwo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		YearsTwo(1)
+	}
+}
+
+func ExampleYears() {
+	fmt.Println(Years(1))
+	// Output:
+	// 7
+}
+
+func ExampleYearsTwo() {
+	fmt.Println(YearsTwo(1))
+	// Output:
+	// 7
+}
 
 func TestYears(t *testing.T) {
 	s := Years(1)
@@ -13,17 +40,5 @@ func TestYearsTwo(t *testing.T) {
 	s := YearsTwo(1)
 	if s != 7 {
 		t.Error("Got", s, "Want", 7)
-	}
-}
-
-func BenchmarkYears(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Years(1)
-	}
-}
-
-func BenchmarkYearsTwo(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		YearsTwo(1)
 	}
 }
